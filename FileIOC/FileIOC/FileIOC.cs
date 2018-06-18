160167cc45037d13feffb061d5c4e36f8ef16da5
Unknown <vanessa.osterberg@outlook.com>
2018-06-18 11:12:44 -0500

    Stuff that isn't important

--- FileIOC/FileIOC/FileIOC.cs
+++ FileIOC/FileIOC/FileIOC.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace FileIOC
 {
@@ -9,7 +7,8 @@ namespace FileIOC
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("What is the name of the file?");
+            String name = "jhghj";
         }
     }
 }
