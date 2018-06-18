5d416fbc304730c94f4ddc067c98bd963554c84d
su20 <19liuq@elmbrookstudents.org>
2018-06-18 11:13:47 -0500

    somework

---
+++ FileIOC/FileIOC/FIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileIOC
+{
+    public class FIO
+    {
+
+        string fileName = Console.ReadLine();
+            using (StreamReader sr = File.OpenText("D:\\repo\\FileIOC\\FileIOC\\FileIOC\\" + fileName + ".txt"))
+            {
+                if (sr.ToString() != null)
+                {
+
+                    string s = "";
+                    do
+                    {
+                        s = sr.ReadLine();
+                        int x = 0;
+    Int32.TryParse(s, out x);
+                        sum += x;
+                    } while (s != null);
+                }
+            }
+
+            using (StreamWriter file = new System.IO.StreamWriter("D:\\repo\\FileIOC\\FileIOC\\FileIOC\\Results.txt"))
+            {
+                file.WriteLine(sum); 
+            }
+
+    }
+}
