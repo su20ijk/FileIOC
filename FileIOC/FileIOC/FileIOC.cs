b1a12514ce629798bef30ea3424047dfa913d86b
su20 <19liuq@elmbrookstudents.org>
2018-06-18 10:00:39 -0500

    initial project

---
+++ FileIOC/FileIOC/FileIOC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileIOC
+{
+    public class FileIOC
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
