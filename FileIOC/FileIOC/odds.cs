c299b533f2b93832bc906a89bdeed813d7d51031
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-19 10:08:52 -0500

    added odds program and test txt file

---
+++ FileIOC/FileIOC/odds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.WriteLine("please enter the name of the file:");
+        string fileName = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(fileName);
+        int[] nums = 
+        int sum = 0;
+        for (int i = 1; i < lines.Length; i += 2)
+        {
+            sum += nums[i]; 
+        }
+
+    }
+}
