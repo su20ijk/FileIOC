5bcfd05f4afb2b91c2cdb91468aa556b8ea6df1d
Unknown <vanessa.osterberg@outlook.com>
2018-06-18 20:11:58 -0500

    tried some stuff, I think it's wrong, though.
    I didn't read the problem thouroughly enough so this stuff here isn't done or completely right yet.

--- FileIOC/FileIOC/FileIOC.cs
+++ FileIOC/FileIOC/FileIOC.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 
 namespace FileIOC
 {
@@ -7,8 +7,22 @@ namespace FileIOC
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("What is the name of the file?");
-            String name = "jhghj";
+            string filename = Console.ReadLine();
+                    if (filename.Length > 5)
+                    {
+                        for (int i = 0; i < filename.Length + 1; i += 5)
+                        {
+                            string x = filename.Remove(i);
+                            if (x.Equals(1) || x.Equals(2) || x.Equals(3) || x.Equals(4) || x.Equals(5) || x.Equals(6) || x.Equals(7) || x.Equals(8) || x.Equals(9))
+                                Console.WriteLine(x);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The file size isn't large enough to display a fifth variable.");
+                    }
+                    Console.Read();
+                
         }
     }
 }
