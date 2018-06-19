1741ab7ddc55289ec52392cd250a33e489bea114
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-19 10:10:34 -0500

    convert string to int

--- FileIOC/FileIOC/odds.cs
+++ FileIOC/FileIOC/odds.cs
@@ -7,11 +7,15 @@ public class Class1
         Console.WriteLine("please enter the name of the file:");
         string fileName = Console.ReadLine();
         string[] lines = System.IO.File.ReadAllLines(fileName);
-        int[] nums = 
+        int[] num = new int[lines.Length];
+        for (int i = 0; i < num.Length; i++)
+        {
+            num[i] = Convert.ToInt32(lines[i]);
+        }
         int sum = 0;
         for (int i = 1; i < lines.Length; i += 2)
         {
-            sum += nums[i]; 
+            sum += num[i]; 
         }
 
     }
