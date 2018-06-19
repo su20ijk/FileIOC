a7fd55c8081f632f627718c22fd05e6195bf64f3
Gene Lee <leexgenex@gmail.com>
2018-06-19 09:02:30 -0500

    dunno if this will work

--- FileIOC/FileIOC/FileIOC.cs
+++ FileIOC/FileIOC/FileIOC.cs
@@ -15,8 +15,26 @@ namespace FileIOC
             Console.WriteLine("please enter the name of the file:");
             string fileName = Console.ReadLine();
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            path += "/"+fileName+".txt";
+            path += "/" + fileName + ".txt";
             string[] lines = System.IO.File.ReadAllLines(path);
+            int[] num = new int[lines.Length];
+            for (int i = 0; i <lines.Length; i++)
+            {
+                num[i] = Convert.ToInt32(lines[i]);
+            }
+            Console.WriteLine("Sum:");
+            int sum = 0;
+            foreach (int x in num)
+            { 
+                sum+=x;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine();
+            Console.WriteLine("Numbers in file:");
+            foreach (int x in num)
+            { 
+                Console.WriteLine(x);
+            }
 
         }
     }
