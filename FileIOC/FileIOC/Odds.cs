661e2f80185f4406eb048e62ba1bde25a6ef07b3
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-21 14:16:25 -0500

    some changes and separate class and backup

---
+++ FileIOC/FileIOC/Odds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace FileIOC
+{
+    public class Odds
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the name of the input file:");
+            string inputFileName = Console.ReadLine();
+            var inPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            inPath += "/" + inputFileName + ".txt";
+            Console.WriteLine("please enter the name of the output file:");
+            string outFileName = Console.ReadLine();
+            var outPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            outPath += "/" + outFileName + ".txt";
+            string[] lines = System.IO.File.ReadAllLines(inPath);
+            System.IO.StreamWriter file = new System.IO.StreamWriter(outPath);
+            int[] nums = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                nums[i] = int.Parse(lines[i]);
+                if ((nums[i] - 1) % 2 == 0)
+                {
+                    file.WriteLine(nums[i]);
+                }
+            }
+            file.Close();
+        }
+    }
+}
--- FileIOC/FileIOC/Odds2.cs
+++ FileIOC/FileIOC/Odds2.cs
@@ -7,23 +7,28 @@ using System.Threading.Tasks;
 
 namespace FileIOC
 {
-    public class FileIOC
+    public class Odds2
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("please enter the name of the input file:");
             string inputFileName = Console.ReadLine();
             var inPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            inPath += "/"+inputFileName+".txt";
+            inPath += "/" + inputFileName + ".txt";
             Console.WriteLine("please enter the name of the output file:");
             string outFileName = Console.ReadLine();
             var outPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             outPath += "/" + outFileName + ".txt";
             string[] lines = System.IO.File.ReadAllLines(inPath);
             System.IO.StreamWriter file = new System.IO.StreamWriter(outPath);
-            foreach(string line in lines)
+            int[] nums = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
             {
-                file.WriteLine(line);
+                nums[i] = int.Parse(lines[i]);
+                if ((nums[i] - 1) % 2 == 0)
+                {
+                    file.WriteLine(nums[i]);
+                }
             }
             file.Close();
         }
